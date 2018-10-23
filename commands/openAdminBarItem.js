2e09af9bec1a14edb1fcc3aeccf121c1db4681aa
Thomas Boutell <tom@punkave.com>
2018-10-23 16:28:11 -0400

    fixed race condition introduced by well-meaning code

--- commands/openAdminBarItem.js
+++ commands/openAdminBarItem.js
@@ -4,36 +4,44 @@ exports.command = function openAdminBarItem(name) {
   // the admin bar succeeds in opening it
 
   return this.waitForNoModals()
+    .execute(function() {
+      $('body').attr('opening-admin-bar-item', "1");
+    })
+    .waitForElementPresent('body[opening-admin-bar-item="1"]')
     .execute(function(name) {
+      $('body').attr('opening-admin-bar-item', "1");
       function attempt() {
         console.log('in attempt');
         if ($(".apos-admin-bar.apos-active").length === 0) {
           $("[data-apos-actionable=data-apos-admin-bar]").click();
           return dropdownAttempt();
+        } else {
+          dropdownAttempt();
         }
-        setTimeout(attempt, 50);
       }
       attempt();
       function dropdownAttempt() {
         console.log('in dropdownAttempt');
         // Might be grouped in a dropdown, if it is toggle that dropdown once its
         // trigger button is visible
-        var $target = $('[data-apos-admin-bar-item="' + name + '"]');
+        var $target = $('.apos-admin-bar [data-apos-admin-bar-item="' + name + '"]');
         console.log('target length is ' + $target.length);
         var $dropdown = $target.closest('[data-apos-dropdown]');
         if (!$dropdown.length) {
           console.log('I believe there is no dropdown');
+          $('body').attr('opening-admin-bar-item', "0");
           return;
         }
         var $trigger = $dropdown.find('.apos-admin-bar-item-inner:visible');
         if ($trigger.length) {
           console.log('clicking trigger');
           $trigger.click();
+          $('body').attr('opening-admin-bar-item', "0");
           return;
         } else {
           console.log('no visible trigger in:', $dropdown[0]);
         }
         setTimeout(dropdownAttempt, 50);
       }
-    }, [ name ]).clickWhenReady('[data-apos-admin-bar-item="' + name + '"]');
+    }, [ name ]).waitForElementNotPresent('body[opening-admin-bar-item="1"]').clickWhenReady('.apos-admin-bar [data-apos-admin-bar-item="' + name + '"]');
 };
--- commands/waitForElementReady.js
+++ commands/waitForElementReady.js
@@ -1,5 +1,8 @@
 exports.command = function waitForElementReady(selector) {  
   return this
   .waitForElementNotPresent('.apos-busy,.apos-global-busy')
-  .waitForElementVisible(selector);
+  .waitForElementVisible(selector)
+  // I have observed click still not being possible right away
+  // even when busy is not present and selector is clearly visible. -Tom
+  .pause(250);
 };
