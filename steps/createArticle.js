e4bf97b3e5f1e840c136f365609d3e281e516c9e
Gregory Dunn <grdunn@gmail.com>
2019-06-17 21:39:58 -0400

    tweak article step on account of arrangeFields change in apostrophe-blog 2.2.1

--- steps/createArticle.js
+++ steps/createArticle.js
@@ -17,7 +17,6 @@ module.exports = (articleName) => {
       const selectPublishedSelector = '.apos-field-published select';
       const inputPublicationDateSelector = '.apos-field-date input';
       const metaTabSelector = '[data-apos-open-group=meta]';
-      const infoTabSelector = '[data-apos-open-group=default]';
       const saveBtnSelector = '[data-apos-save]';
       const manageTableRowSelector = '.apos-manage-table tr[data-piece]';
 
@@ -27,7 +26,6 @@ module.exports = (articleName) => {
       client.resetValueInModal('apostrophe-blog-editor-modal', inputTitleSelector, articleName);
       client.clickInModal('apostrophe-blog-editor-modal', metaTabSelector);
       client.resetValueInModal('apostrophe-blog-editor-modal', selectPublishedSelector, 'Yes');
-      client.clickInModal('apostrophe-blog-editor-modal', infoTabSelector);
 
       const currentDate = new Date();
       const day = castTwoDigits(currentDate.getDate());
